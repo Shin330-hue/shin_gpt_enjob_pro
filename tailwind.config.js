30c2f64975defbf8d955698cb589a12de661870f
Shin <sarada33379@gmail.com>
2025-05-17 10:56:08 +0900

    Add web UI with black text

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: [
+    './index.html',
+    './main.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        playfulBlue: '#60a5fa',
+        playfulPink: '#fb7185',
+        playfulGreen: '#4ade80',
+        win: '#22c55e',
+        lose: '#ef4444',
+        draw: '#facc15'
+      }
+    }
+  },
+  plugins: []
+}
